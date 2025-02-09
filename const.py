c6dca192341b71da4b9757fc99da5ed210e6b3da
Lobel Marunić <146864634+Mramar-croatia@users.noreply.github.com>
2025-02-09 19:17:16 +0100

    In this commit I added a k.py file

--- const.py
+++ const.py
@@ -1,6 +1,6 @@
 BACKGROUND_COLOR = "white" # Background color of the screen
 GRID_LINE_WIDTH = 3 # Width of the grid lines
-SQUARE_SIZE = 20 # Square size in pixels, it has to be a multiple of 20
+SQUARE_SIZE = 40 # Square size in pixels, it has to be a multiple of 20
 
 START_PAUSED = True # Whether the game starts paused
 CHANCE = 4 # Chance of a cell being alive when generating a random grid (STANDARD: 7)
